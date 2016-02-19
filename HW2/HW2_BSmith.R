6f477aba7a3a7abb6c20b4235869b8672d490b64
BrendanSmith <smithbrendanjames@gmail.com>
2016-02-18 22:39:37 -0800

    HW2 Final commit

--- HW2/HW2_BSmith.R
+++ HW2/HW2_BSmith.R
@@ -1,7 +1,11 @@
+library(plyr)
+library(ggplot2)
+library(OpenStreetMap)
+library(rgdal)
 
 ripdata <- read.csv("./riparian_survey.csv",header = TRUE, sep = ",")
-str(ripdata)
-summary(ripdata)
+#str(ripdata)
+#summary(ripdata)
 
 #Assign Project Codes given the Project IDs in ripdata to something easier to parse through
 nID <- c("COSRP", "HEROW","NAPSO", "SACTO")
@@ -17,7 +21,7 @@ ripdata <- ripdata[!(ripdata$SpeciesVarietalCode %in% c("UNKOWN", "DEAD", "Not R
 ripdata <- ripdata[ripdata$Woody_DBH_cm > 0, ]
 ripdata <- ripdata[ripdata$Woody_Height_m > 0, ]
 
-#ripdata <- droplevels(ripdata)
+#ripdata <- droplevel s(ripdata)
 
 ProjLoc <- aggregate(cbind(Longitude,Latitude) ~ ProjCode,data=ripdata, mean)
 MeasData <- aggregate(cbind(Woody_DBH_cm,Woody_Height_m) ~ ProjCode,data=ripdata, mean)
@@ -35,4 +39,26 @@ rm(x)
 ssgfreq <- head(gfreq,5)
 
 #Step 3 - Test for Independence
+#Frequency as a function of Genus and Location
+ssripdata<-ripdata[ripdata$Genus %in% ssgfreq$Genus,]
+twrip <- table(ssripdata$Genus,ssripdata$ProjCode)
 
+crripdata <- chisq.test(twrip)
+
+platlon <- subset(ProjLoc, select = c("Latitude","Longitude"))
+#plot(platlon,col=0)
+#points(platlon, pch=c(5,2,4,3))
+
+#I tried getting the State map to work, but to no avail
+#map <- openmap(c(max(ProjLoc$Latitude)+1,min(ProjLoc$Longitude)-1), c(min(ProjLoc$Latitude)-1,max(ProjLoc$Longitude)+1))
+#plot(map)
+#x <- SpatialPoints(platlon)
+#x.wgs84 <-spTransform(x, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+#osm <- openmap (c(bbox(x.wgs84)[2,2] + 1, bbox(x.wgs84)[1,1] - 1), c(bbox(x.wgs84)[2,1] - 1, bbox(x.wgs84)[1,2] + 1))
+#osm.euref <- openproj (osm, proj4string(x))
+#plot(osm.euref)
+#plot(x,add=T,pch=20)
+
+#Use the write.csv function to save my new dataframe with only
+#the five most frequent genera
+write.csv(ssripdata,file = "newripdata_survey.csv")
